a9f210eee2e042534f75ca11dcbb7d361aae9201
youssef <xyoussefx1500@gmail.com>
2024-09-10 01:35:22 +0200

    added comments plugin and systembuffershare

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,3 +9,9 @@ vim.opt.number = true
 
 -- clipboard support
 vim.opt.clipboard = "unnamedplus"
+
+-- Copy selection to system clipboard
+vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent })
+
+-- Paste from system clipboarhd
+vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true, silent = true })
