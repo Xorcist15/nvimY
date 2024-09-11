f8ef47b74e452f044efac2d0df40c7536f694235
youssef <xyoussefx1500@gmail.com>
2024-09-12 01:15:23 +0200

    added lualine, needs config

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,34 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+  config = function()
+    require('lualine').setup {
+      options = {
+        icons_enabled = true,
+        theme = 'gruvbox',
+        component_separators = { '', '' },
+        section_separators = { '', '' },
+        disabled_filetypes = {}
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+      extensions = {}
+    }
+  end
+}
+
