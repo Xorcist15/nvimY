56c0f163e106ce6f9cfc9145306ff3c5ef226f3c
youssef <xyoussefx1500@gmail.com>
2024-09-12 21:50:57 +0200

    nothing

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,7 +11,7 @@ vim.opt.number = true
 vim.opt.clipboard = "unnamedplus"
 
 -- Copy selection to system clipboard
-vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent })
+vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true })
 
 -- Paste from system clipboarhd
 vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true, silent = true })
