dc31ab206edcb9d4e8e086aa2bbc92ba60dc0987
youssef <xyoussefx1500@gmail.com>
2024-11-09 18:42:06 +0000

    Added terminal

--- nvim/lua/plugins/nvim-toggleterm.lua
+++ nvim/lua/plugins/nvim-toggleterm.lua
@@ -1,3 +1,11 @@
 return {
-  'akinsho/nvim-toggleterm.lua',
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    opts = {},
+    config = function()
+      require("toggleterm").setup {}
+      vim.api.nvim_set_keymap('n', '<leader>t', ':ToggleTerm<CR>', { noremap = true, silent = true })
+    end
+  }
 }
